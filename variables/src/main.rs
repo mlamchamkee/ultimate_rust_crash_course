fb070426e62ca2e024a0acc7fdd34297b1b496ed
Michael Lam <mlamchamkee@gmail.com>
2024-08-03 13:45:24 -0400

    exercise a: variables

---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+fn main() {
+    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+//     let _sum = missiles + ready;
+//     READY_AMOUNT -= 1;
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles -= ready;
+    println!("{} missiles left", missiles - ready);
+}
