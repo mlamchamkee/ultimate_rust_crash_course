06988dd9cc1fb7a33e6c65e77212e6dde72a7014
Michael Lam <mlamchamkee@gmail.com>
2024-08-04 11:54:47 -0400

    exercise d: control flow

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -17,8 +17,17 @@ fn main() {
         // 1a. Your task: handle the command-line arguments!
         //
         // - If arg is "sum", then call the sum() function
+        if arg == "sum" {
+            sum();
+        }
         // - If arg is "double", then call the double() function
+        else if arg == "double" {
+            double();
+        }
         // - If arg is anything else, then call the count() function, passing "arg" to it.
+        else {
+            count(arg);
+        }
 
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
@@ -32,6 +41,9 @@ fn sum() {
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
 
+    for i in 7..=23 {
+        sum += i;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -42,7 +54,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-
+    while x * 2 <= 500 {
+        count += 1;
+        x *= 2;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
@@ -51,8 +66,14 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut count = 0;
+    loop {
+        if count >= 8 {
+            break;
+        }
+        count += 1;
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
