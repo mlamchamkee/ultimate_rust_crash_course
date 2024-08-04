f1324b3d27a18d24b63660806ab10dc9f5b40105
Michael Lam <mlamchamkee@gmail.com>
2024-08-04 13:28:30 -0400

    exercise g: collections and enums

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -10,14 +10,26 @@
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
+enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss,
+}
+
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
         // 1b. Implement this method to convert a Shot into points
-        // - return 5 points if `self` is a `Shot::Bullseye`
-        // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
-        // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
-        // - return 0 points if `self` is a Miss
+        match self {
+            // - return 5 points if `self` is a `Shot::Bullseye`
+            Shot::Bullseye => 5,
+            // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
+            Shot::Hit(x) if x < 3.0 => 2,
+            // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
+            Shot::Hit(x) => 1,
+            // - return 0 points if `self` is a Miss
+            Shot::Miss => 0,
+        }
     }
 }
 
@@ -34,10 +46,21 @@ fn main() {
     //      - Less than 1.0 -- `Shot::Bullseye`
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
-
+    for coord in arrow_coords {
+        coord.print_description();
+        let shot = match coord.distance_from_center() {
+            v if v < 1.0 => Shot::Bullseye,
+            v if v <= 5.0 => Shot::Hit(v),
+            _ => Shot::Miss,
+        };
+        shots.push(shot);
+    }
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
+    for shot in shots {
+        total += shot.points();
+    }
 
     println!("Final point total is: {}", total);
 }
